228af09b725898fbf35674d3cef4bba4e3314ac3
jstrombe19 <jaredstromberg@gmail.com>
2020-05-13 11:29:53 -0600

    Defined create action in Buses controller. Verified via Postman.

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -9,12 +9,20 @@ class BusesController < ApplicationController
   end
 
   def create
+    @bus = Bus.create(bus_params)
+    render json: @bus
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def bus_params
+    params.require(:bus).permit(:tag, :driver, :current_route, :capacity)
   end
 
 end
